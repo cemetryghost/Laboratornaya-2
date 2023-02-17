3108bed30c4f60b552af4d73df1ae8ede1d906be
cemetryghost <96484941+cemetryghost@users.noreply.github.com>
2023-02-17 11:22:23 +0300

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите размерность массива: ");
+
+        int dimension = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[dimension];
+
+        Console.WriteLine("Введите элементы массива: ");
+
+        for (int i = 0; i < dimension; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int k = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > k)
+            {
+                arr[j + 1] = arr[j];
+                arr[j] = k;
+                j--;
+            }
+        }
+
+        Console.Write("Сортировка: ");
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + ", ");
+        }
+
+    }
+}
